6ec77de7330c8f0dba3c45f867ac7a640b74cc3c
Guillaume Bagan <gbagan@gmail.com>
2025-09-29 17:56:53 +0200

    supprime katexity.ts

--- src/katexify.ts
+++
@@ -1,9 +0,0 @@
-import katex, { type KatexOptions } from "katex";
-export default function katexify(math: string, displayMode: boolean = false) {
-  const options: KatexOptions = {
-	  displayMode: displayMode,
-	  throwOnError: false,
-    output: "html"
-  };
-  return katex.renderToString(math, options);
-}
